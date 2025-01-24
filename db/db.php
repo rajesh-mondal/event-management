15e34969e9c6a4a74b05e1725f4c0f0595cf9d8e
Rajesh Mondal <rajesh01749@gmail.com>
2025-01-25 00:55:38 +0600

    Establish database connection

---
+++ db/db.php
@@ -0,0 +1,12 @@
+<?php
+
+include '../config.php';
+
+$conn = new mysqli( DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME );
+
+// Check the connection
+if ( $conn->connect_error ) {
+    die( "Connection failed: " . $conn->connect_error );
+} else {
+    echo "Database Connected!";
+}
